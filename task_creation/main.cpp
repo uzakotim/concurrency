7f0a39d72910e47e6c5589f3f41d9f6f71583c09
Bc. Timur Uzakov <uzakotim@fel.cvut.cz>
2021-09-26 22:18:25 +0500

    kernelTimeSlicing attempt

--- task_creation/main.cpp
+++ task_creation/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <thread>
-// #include <taskLib.h>
+#include <linux/sched.h>
 #include <stdio.h>
-// #include <kernelLib.h>
 
 long int task_run[]={100, 450, 200};
 int task_stop[]={18, 25, 30};
@@ -12,6 +11,8 @@ void task(int n)
         long int x;
 
         printf("Task %i has been started\n", n);
+
+        
 
         while (1)
         {
@@ -23,18 +24,34 @@ void task(int n)
         }
 }
 
+void kernelTimeSlicing()
+{
+        struct sched_param sp = {
+        .sched_priority = 90
+        };
+        pid_t pid = 0;
+        sched_setscheduler(pid, SCHED_RR, &sp);
+        // sched_setscheduler(pid, SCHED_OTHER, &sp);
+}
+
+
 void CreateTasks(void)
 {
-        /*  kernelTimeSlice(1); */
+        
+        kernelTimeSlicing();
         std::thread id1(task,0);
         std::thread id2(task,1);
         std::thread id3(task,2);
+
         id1.join();
         id2.join();
         id3.join();
 }
 
+
+
 int main()
 {
-    CreateTasks();    
+        CreateTasks();    
 }
+
